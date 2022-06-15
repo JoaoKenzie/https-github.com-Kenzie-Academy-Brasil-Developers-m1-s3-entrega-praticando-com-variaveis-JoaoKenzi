e3e944df279ed85ee59c95f47eb7c46a564d0e5f
João Paulo Oliveira Barreto <106703587+JoaoKenzie@users.noreply.github.com>
2022-06-15 00:47:45 -0300

    entrega

---
+++ index.js
@@ -0,0 +1,7 @@
+let nome = "João Paulo";
+let sobrenome = "Barreto";
+let anoNascimento = 1999;
+let cidade = "Imperatriz"
+let anoAtual = 2022;
+let idade = anoAtual - anoNascimento
+console.log(`Eu sou o ${nome} ${sobrenome} da cidade de ${cidade} e tenho ${idade} anos de idades.`)
